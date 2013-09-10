4a936c3d8a9b4615e6f739d353b14eeb058efac6
yubin <1960028060@qq.com>
2013-09-10 08:54:01 +0800

    repair window split

--- init-local.el
+++ init-local.el
@@ -49,67 +49,6 @@
    )
  )
 
-;  +----------------------+                 +------------+-----------+
-;  |                      |           \     |            |           |
-;  |                      |   +-------+\    |            |           |
-;  +----------+-----------+   +-------+/    |            +-----------+
-;  |          |           |           /     |            |           |
-;  |          |           |                 |            |           |
-;  +----------+-----------+                 +------------+-----------+
-(defun split-v-3 ()
-  "Change 3 window style from horizontal to vertical"
-  (interactive)
-
-  (select-window (get-largest-window))
-  (if (= 3 (length (window-list)))
-      (let ((winList (window-list)))
-            (let ((1stBuf (window-buffer (car winList)))
-                  (2ndBuf (window-buffer (car (cdr winList))))
-                  (3rdBuf (window-buffer (car (cdr (cdr winList))))))
-              (message "%s %s %s" 1stBuf 2ndBuf 3rdBuf)
-
-              (delete-other-windows)
-              (yubin/split-window-h)
-              (set-window-buffer nil 1stBuf)
-              (other-window 1)
-              (set-window-buffer nil 2ndBuf)
-              (yubin/split-window-v)
-              (set-window-buffer (next-window) 3rdBuf)
-              (select-window (get-largest-window))
-            ))))
-
-;  +------------+-----------+                  +----------------------+
-;  |            |           |            \     |                      |
-;  |            |           |    +-------+\    |                      |
-;  |            +-----------+    +-------+/    +----------+-----------+
-;  |            |           |            /     |          |           |
-;  |            |           |                  |          |           |
-;  +------------+-----------+                  +----------+-----------+
-
-
- (defun split-h-3 ()
-  "Change 3 window style from vertical to horizontal"
-  (interactive)
-
-  (select-window (get-largest-window))
-  (if (= 3 (length (window-list)))
-      (let ((winList (window-list)))
-            (let ((1stBuf (window-buffer (car winList)))
-                  (2ndBuf (window-buffer (car (cdr winList))))
-                  (3rdBuf (window-buffer (car (cdr (cdr winList))))))
-                 (message "%s %s %s" 1stBuf 2ndBuf 3rdBuf)
-
-                 (delete-other-windows)
-                 (yubin/split-window-v)
-                 (set-window-buffer nil 1stBuf)
-                 (other-window 1)
-                 (set-window-buffer nil 2ndBuf)
-                 (yubin/split-window-h)
-                 (set-window-buffer (next-window) 3rdBuf)
-                 (select-window (get-largest-window))
-              ))))
-
-
 ;  +------------+-----------+                 +------------+-----------+
 ;  |            |           |            \    |            |           |
 ;  |            |           |    +-------+\   |            |           |
@@ -153,10 +92,10 @@
                   (split-type-2 nil)
                   )
               (if (= (window-width) (frame-width))
-                  (setq split-type-1 'yubin/split-window-h
-                        split-type-2 'yubin/split-window-v)
-                (setq split-type-1 'yubin/split-window-v
-                       split-type-2 'yubin/split-window-h))
+                  (setq split-type-1 'split-window-horizontally
+                        split-type-2 'split-window-vertically)
+                (setq split-type-1 'split-window-vertically
+                      split-type-2 'split-window-horizontally))
               (funcall split-3 1stBuf 2ndBuf 3rdBuf split-type-1 split-type-2)
 
  ))))
@@ -202,5 +141,15 @@
                   (set-window-buffer 3rdWin 1stBuf))
                 ))))))
 
+;显示行号
+(global-linum-mode 1)
+
+;打开文件默认设置为只读
+(defun make-some-files-read-only ()
+  "when file opened is of a certain mode, make it read only"
+  (when (memq major-mode '(c++-mode tcl-mode text-mode python-mode emacs-lisp-mode))
+    (toggle-read-only 1)))
+
+(add-hook 'find-file-hooks 'make-some-files-read-only)
 
 (provide 'init-local)
